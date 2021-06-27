045b512145e9a266d9b3572913ec39c31f825a74
Darx <snehankobose31@gmail.com>
2021-06-27 09:08:14 +0530

    Added Login.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
 import Mail from "./Component/Mail";
+import Login from "./Component/Login";
+
 
 function App() {
   return (
     <div>
         <Mail />
+        <div>
+          <h3>Click here to login</h3>
+            <Login />
+          </div>
     </div>
   );
 }
--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import GoogleLogin from "react-google-login";
+import axios from 'axios';
 
 const Login = () => {
+
+    const responseSuccessGoogle =(response)=>{
+        console.log(response);
+        axios({
+            method:"POSt",
+            url:"http://localhost:3000/api/googlelogin",
+            data:{tokenId:response.tokenId}
+        }).then(response=>{
+            console.log(response)
+        })
+    }
+
+    const responseErrorGoogle =(response)=>{
+        
+    }
+
     return (
         <div>
-            
+            <GoogleLogin
+                clientId="99166491960-gf19sgna4kc892ec6vl6htdf9dsh7o31.apps.googleusercontent.com"
+                buttonText="Login With Google"
+                onSuccess={responseSuccessGoogle}
+                onFailure={responseErrorGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
         </div>
     )
 }
